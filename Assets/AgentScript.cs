db652af0c04fbbfefe4db1552aaa0c47a6bd09b1
Symon <simonrigard@gmail.com>
2018-04-02 02:33:57 +0200

    Added working pathfinding

---
+++ Assets/AgentScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentScript : MonoBehaviour {
+
+    NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Input.GetButtonDown("Fire1"))
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                if (hit.collider.CompareTag("Walkable"))
+                    agent.SetDestination(hit.transform.position);
+            }
+
+    }
+}
