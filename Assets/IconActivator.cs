4447e6dabdb4b36f89e6b8b8ab710ec0258a25fe
Symon <simonrigard@gmail.com>
2018-04-23 20:23:40 +0200

    Add menu icons + prefabs

---
+++ Assets/IconActivator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IconActivator : MonoBehaviour {
+
+	public GameObject rainObject;
+	public GameObject snowObject;
+	public bool rain;
+	public bool snow;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (rain && !snow)
+		{
+			rainObject.SetActive(true);
+			snowObject.SetActive(false);
+
+		}
+		else if (snow && !rain)
+		{
+			snowObject.SetActive(false);
+			snowObject.SetActive(true);
+		}
+		else if (snow && rain)
+		{
+			rainObject.SetActive(true);
+			snowObject.SetActive(true);
+			snowObject.transform.position = new Vector3(snowObject.transform.position.x - 6.3f, snowObject.transform.position.y, snowObject.transform.position.z);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
